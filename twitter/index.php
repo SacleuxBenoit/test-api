a03b7909b86d0de36f4104716d5ddc430f701a55
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-06-21 17:56:35 +0200

    display timeline of my twitter :arrow_right: twitter/index.php

---
+++ twitter/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<a class="twitter-timeline" href="https://twitter.com/Benoit_dev_?ref_src=twsrc%5Etfw">Tweets by Benoit_dev_</a> 
+<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+</body>
+</html>
